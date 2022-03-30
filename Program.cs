07357dbac14c92749547bb56b3a285dad127a1d7
Stephen Madalo <stephenmadalo89@gmail.com>
2022-03-30 12:20:09 -0400

    tasks done

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace variablePracticeConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string myName;
+            bool isAlive;
+            char favoriteLetter;
+            double favoriteNumber;
+            decimal bankAccountAmount;
+
+            age = 22;
+            myName = "auugggh";
+            isAlive = false;
+            favoriteLetter = 'S';
+            favoriteNumber = 9;
+            bankAccountAmount = 0.89m;
+
+            Console.WriteLine($"Hi, my name is {myName}, I am {age} years old. My favorite number and letter is {favoriteNumber} & {favoriteLetter}, respectively.");
+            Console.WriteLine($"I have {bankAccountAmount} in my bank account. Am I really living? {isAlive}");
+        }
+    }
+}
